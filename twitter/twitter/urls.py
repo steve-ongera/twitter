87410259971e2c19f6835b2108ed2a63b296f8d3
steve-ongera <gadafisteve001@gmail.com>
2026-03-26 21:37:01 +0300

    made updates

--- twitter/twitter/urls.py
+++ twitter/twitter/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('core.urls')),
+    path('api/', include('core.urls')),
 ]
 
 if settings.DEBUG:
